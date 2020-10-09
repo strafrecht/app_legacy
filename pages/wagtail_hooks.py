301d43c98ada6c6aa689ee9c7508b3bbe9a79ab1
Admin <admin@ub-10-126-71-56.eduroam-ub.privat>
2020-10-09 17:52:19 +0200

    fixed responsive columns css

--- pages/wagtail_hooks.py
+++ pages/wagtail_hooks.py
@@ -1,9 +1,11 @@
+from django.utils.html import format_html
+from django.templatetags.static import static
+from wagtail.admin.rich_text.converters.html_to_contentstate import InlineStyleElementHandler, BlockElementHandler, InlineEntityElementHandler
+from wagtail.core import hooks
+import wagtail.admin.rich_text.editors.draftail.features as draftail_features
 from wagtail.contrib.modeladmin.options import (
     ModelAdmin, ModelAdminGroup, modeladmin_register
 )
-import wagtail.admin.rich_text.editors.draftail.features as draftail_features
-from wagtail.admin.rich_text.converters.html_to_contentstate import InlineStyleElementHandler, BlockElementHandler, InlineEntityElementHandler
-from wagtail.core import hooks
 from pages.models import People, NodeAdmin, Events, Sessions, Exams
 from wagtailpolls.models import Poll
 
@@ -49,6 +51,14 @@ modeladmin_register(PollsModelAdmin)
 modeladmin_register(ExamsModelAdmin)
 
 # pages/news/polls/poll-eval/sessions/events/people/newsletter email/
+
+@hooks.register("insert_global_admin_css", order=100)
+def global_admin_css():
+    return format_html(
+        '<link rel="stylesheet" href="{}">',
+        static("css/columns.css")
+    )
+
 @hooks.register('construct_main_menu')
 def hide_snippets_menu_item(request, menu_items):
   for item in menu_items: print("XXX: {}".format(item.name))
@@ -58,6 +68,8 @@ def hide_snippets_menu_item(request, menu_items):
 def register_neu_feature(features):
     feature_name = 'neulabel'
     type_ = 'NEULABEL'
+    tag = 'NEU'
+
     control = {
         'type': type_,
         'label': 'NEU',        
@@ -67,25 +79,24 @@ def register_neu_feature(features):
             'color': '#666'        
         }    
     }
+
     features.register_editor_plugin(        
         'draftail',
         feature_name,
         draftail_features.InlineStyleFeature(control)    
     )     
+
     db_conversion = {        
-        'from_database_format': {
-            'span[class="label neu"]':
-                   InlineStyleElementHandler(type_)
-        },        
-        'to_database_format': {
-            'style_map': {type_: 'span class="label neu"'}
-        },    
+        'from_database_format': {'NEU': InlineStyleElementHandler(type_)},        'to_database_format': {'block_map': {type_: 'span class="label neu"'}},    
     }     
+
     features.register_converter_rule(
         'contentstate',
         feature_name,
         db_conversion
     )
+
+    features.default_features.append('n')
 
 @hooks.register('register_rich_text_features')
 def register_ueberarbeitet_feature(features):
@@ -152,3 +163,32 @@ def register_roofline_feature(features):
         feature_name,
         db_conversion
     )
+
+@hooks.register('register_rich_text_features')
+def register_blockquote_feature(features):
+    """
+    Registering the `blockquote` feature, which uses the `blockquote` Draft.js block type,
+    and is stored as HTML with a `<blockquote>` tag.
+    """
+    feature_name = 'blockquote'
+    type_ = 'blockquote'
+    tag = 'blockquote'
+
+    control = {
+        'type': type_,
+        'label': '❝',
+        'description': 'Blockquote',
+        # Optionally, we can tell Draftail what element to use when displaying those blocks in the editor.
+        'element': 'blockquote',
+    }
+
+    features.register_editor_plugin(
+        'draftail', feature_name, draftail_features.BlockFeature(control)
+    )
+
+    features.register_converter_rule('contentstate', feature_name, {
+        'from_database_format': {tag: BlockElementHandler(type_)},
+        'to_database_format': {'block_map': {type_: tag}},
+    })
+
+    features.default_features.append('blockquote')
