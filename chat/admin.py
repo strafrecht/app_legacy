81a2737dc183e12ad248181b4952aa4bec247249
Sergio <sfvsound@gmail.com>
2021-05-06 14:53:02 +0200

    session admin

--- chat/admin.py
+++ chat/admin.py
@@ -8,6 +8,6 @@ from django.contrib.sessions.models import Session
 class SessionAdmin(ModelAdmin):
     def _session_data(self, obj):
         return obj.get_decoded()
-    list_display = ['', 'session_key', '_session_data', 'expire_date']
+    list_display = ['session_key', '_session_data', 'expire_date']
 
 admin.site.register(Session, SessionAdmin)
